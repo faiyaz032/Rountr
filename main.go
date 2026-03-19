90345f3bac7dea2e3c758c36e0f2cd0d658e55b6
Faiyaz <faiyazrahman03@gmail.com>
2026-03-19 15:09:03 +0600

    feat: implement initial proxy to the backend server

--- main.go
+++ main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+const BACKEND_ADDR = "localhost:7777"
 
 func main() {
-	fmt.Println("Hello, Rountr!")
+	listener, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+		return
+	}
+	defer listener.Close()
+	fmt.Println("Rountr listening on :8888")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection: ", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("New connection from ", conn.RemoteAddr())
+
+	backendConn, err := net.Dial("tcp", BACKEND_ADDR)
+	if err != nil {
+		log.Println("Error connecting to backend: ", err)
+		return
+	}
+	defer backendConn.Close()
+
+	done := make(chan struct{}, 2)
+
+	go func() {
+		_, err := io.Copy(backendConn, conn)
+		if err != nil {
+			log.Println("Error copying to backend:", err)
+		}
+		done <- struct{}{}
+	}()
+
+	go func() {
+		_, err := io.Copy(conn, backendConn)
+		if err != nil {
+			log.Println("Error copying from backend:", err)
+		}
+		done <- struct{}{}
+	}()
+
+	<-done
+	<-done
+	fmt.Println("Connection from ", conn.RemoteAddr(), " closed")
 }
